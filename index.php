8038290cda2e4819c7d6bf5ee258f460ff6439d7
Orest Khomitskyi <orestkhomitskyi@Orests-MBP.Dlink>
2020-02-01 19:32:49 +0200

    ada

--- index.php
+++ index.php
@@ -53,7 +53,7 @@ try {
     if ($update->message->photo) {
         $file_id = $update->message->photo[0]->file_id;
 
-        error_log("Photo object".serialize($update->message->photo[0]['file_id']));
+        error_log("Photo object".serialize($update->message->photo[0]));
         error_log("FileID: ".$file_id);
         
         $fileInfo = json_decode(file_get_contents("https://api.telegram.org/bot399359167:AAG77kgiiHyAjTt37Y-oi8sGI64w1X89FdU/getFile?file_id".$file_id), TRUE);
